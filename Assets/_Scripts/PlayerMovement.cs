15579191552bdcdb974a722a805c8199da8521e9
Alexander Evstratov <alexdrugev@me.com>
2023-03-23 13:07:20 +0300

    Add player movement.

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _xBorder = 1.75f;
+    private float _speed = 2.0f;
+    private bool _direction;
+
+    private void ChangeDirection()
+    {
+        _direction = !_direction;
+    }
+
+    private void MovePlayer()
+    {
+        if (_direction)
+            transform.Translate(Vector2.right * _speed * Time.deltaTime);
+        else
+            transform.Translate(Vector2.left * _speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || transform.position.x >= _xBorder || transform.position.x <= -_xBorder)
+        {
+            ChangeDirection();
+        }
+    }
+}
